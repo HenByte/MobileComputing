bbe3d211aca5fb6c683864bed0e81304b9a6b38e
He Au <email>
2026-01-19 21:42:03 +0200

    hw1

---
+++ HW1/MobileComputing/app/src/main/java/com/example/mobilecomputing/SampleData.kt
@@ -0,0 +1,64 @@
+package com.example.mobilecomputing
+
+/**
+ * SampleData for Jetpack Compose Tutorial
+ */
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Hippo",
+            "Testing I´m a happy hippo and I am learning Android studio"
+        ),
+        Message(
+            "Hippo",
+            "I like to drink my coffee and watch some youtube at the same time"
+        ),
+        Message(
+            "Hippo",
+            """I think Kotlin is my favorite programming language.
+            |It's so much fun!""".trim()
+        ),
+        Message(
+            "Hippo",
+            "I think I drink my coffee now"
+        ),
+        Message(
+            "Hippo",
+            """After drinking my coffee I´m gonna take a look at Jetpack Compose, it's great!
+            """.trim()
+        ),
+        Message(
+            "Hippo",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "Hippo",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "Hippo",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "Hippo",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "Hippo",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "Hippo",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "Hippo",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Hippo",
+            "This is my first assignment for mobile computing?"
+        ),
+    )
+}
